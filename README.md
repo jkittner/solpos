80329e5125f3afe3d28fb75771645a587867fdcd
Jonas Kittner <jonas.kittner@rub.de>
2022-10-26 15:31:43 +0200

    rename c file add pre-commit.ci

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+[![pre-commit.ci status](https://results.pre-commit.ci/badge/github/theendlessriver13/solpos/master.svg)](https://results.pre-commit.ci/latest/github/theendlessriver13/solpos/master)
+
 # solpos
 
 **Work in progress**
--- setup.py
+++ setup.py
@@ -2,7 +2,10 @@ from setuptools import Extension
 from setuptools import setup
 
 setup(
-    name='solpos',
-    version='0.1.0',
-    ext_modules=[Extension(name='_solpos', sources=['main.c', 'solpos.c'])],
+    ext_modules=[
+        Extension(
+            name='_solpos',
+            sources=['solpos_cpy.c', 'solpos.c'],
+        ),
+    ],
 )
